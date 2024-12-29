b25b1dd0b36fd8764a5c8f662dafd8f0e7c6bb60
Saad Khalid <43319921+Saadkhalid913@users.noreply.github.com>
2024-12-28 23:17:46 -0500

    save

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup, find_packages
+import os
+
+# Ensure README.md is available for long description
+long_description = ''
+if os.path.exists('README.md'):
+    with open('README.md', 'r', encoding='utf-8') as fh:
+        long_description = fh.read()
 
 setup(
     name='web2md',
     version='0.1.0',
     description='Convert web pages to clean, readable Markdown',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     author='Saad Khalid',
     author_email='saadkhalid913+dev@gmail.com',
@@ -25,6 +32,6 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-    ],
-) 
+        'Programming Language :: Python :: 3.10'
+    ]
+)
